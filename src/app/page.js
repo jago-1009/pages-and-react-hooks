b40769a15552d2a6bc41f5206d210a64b730e7bf
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-09-18 11:23:00 -0400

    Began Pokemon Page
    Finished functionality on Home Page, began Pokemon Page, and set up initial scaffolding and basic search functionality.

--- src/app/page.js
+++ src/app/page.js
@@ -2,36 +2,23 @@
 import Image from "next/image";
 import react, {useState, useEffect, useRef, ref, sessionStorage} from 'react';
 export default function Home() {
-
 const [score, setScore] = useState(0)
-const [name, setName] = useState('')
-const nameRef = useRef();
-
-useEffect(() => {
-  setName('User')
-}, [])
-function nameSet(query) { 
-  if (query) {
-    setName(query)}
-}
   function addtoScore() {
     setScore(score + 1)
     console.log(score)
   }
-  
+
+  function doubleScore() {
+    addtoScore()
+  }
   return (
     <main>
-      <h1>Welcome, {name}</h1>
       <h1>Home</h1>
       <div>
         <button onClick={addtoScore}>Add 1</button>
+        <button onClick={doubleScore}>Add 2</button>
         <p>Score: {score}</p>
       </div>
-
-      <form onSubmit={nameSet}>
-      <input className="nameInput" type="text" ref={nameRef}/>
-      <button type="button" onClick={nameSet()}>Set Name</button>
-      </form>
     </main>
   );
 }
--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -1,9 +1,49 @@
+'use client'
+import { useEffect, useState } from 'react'
+import pokeStyles from './pokemon.module.css'
+//Pokemon Data
+const pokeApiURL = 'https://pokeapi.co/api/v2/pokemon/'
+/**
+ * @typedef {Object} pokemonApiObject This is the object for the pokemon api response
+ * @prop {String} name name of Pokemon
+ * @prop {Object} sprites Object with sprite references
+ * @prop {String} sprites.front_default front default sprite
+ * @prop {Object} cries Object with cry references
+ * @prop {string} cries.latest latest cry sound effect
+ * @prop {Number} height height of Pokemon. Multiply by 10 to make it in cms
+ * @prop {Number} weight weight of Pokemon. Divide by 10 to make it in kgs.
+ * 
+ * 
+ * @returns {pokemonApiObject}
+ */
+
 export default function Pokemon() {
+    /**
+     * @type {[pokemonApiObject, Function]}
+     */
+const [pokemon, setPokemon] = useState({})
+/**
+ * @type {[String, Function]}
+ */
+const [searchTerm, setSearchTerm] = useState('')
+
+function changeSearchTerm(event) {
+    setSearchTerm(event.currentTarget.value)
+   
+}
+useEffect(() => {
+    console.log(searchTerm);
+  }, [searchTerm]); 
 return (
-<main>
-<h1>Pokemon Page</h1>
+<main className={pokeStyles.main}>
+<h1 className={pokeStyles.title}>Pokemon Page</h1>
+<div className={pokeStyles.search}>
+    <input type="search" id="search" name="search" placeholder='search for pokemon...' value={searchTerm} onChange={changeSearchTerm}/>
+    <input type="button" value="Search" name="searchButton" />
+</div>
+<div className='pokemonResult'>
 
-
+</div>
 </main>
 
 
